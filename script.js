6301350e6d4f3fbfce6d1b53d81821055506932f
Daniel Nijdl <Dnijdl267@west-mec.local>
2021-04-09 13:52:15 -0700

    Added some stuff to the js file

---
+++ script.js
@@ -0,0 +1,21 @@
+// All pages
+const charmander = require( 'charmader.json' );
+const charmeleon = require( 'charmelion.json' );
+const charizard = require( 'charizard.json' );
+
+
+
+
+// Charmander
+
+
+
+
+
+// Charmeleon
+
+
+
+
+
+// Charizard
